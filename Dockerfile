331fd19653527a1b29631a50ad986586b0f2b188
sachinrajpurohit <sachinrajpurohit1510@gmail.com>
2026-09-02 00:01:06 +0530

    it is first time creating cicd

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM  eclipse-temurin:17-jdk as builder
+
+WORKDIR /build
+
+COPY . .
+
+RUN javac Main.java
+
+
+FROM eclipse-temurin:17-jre-alpine
+
+WORKDIR /app
+
+COPY --from=builder /build/Main.class .
+
+CMD ["java", "Main"]
---
+++ Main.java
@@ -0,0 +1,25 @@
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // Port 5000 par server start karein
+        HttpServer server = HttpServer.create(new InetSocketAddress(5000), 0);
+        server.createContext("/", new HttpHandler() {
+            @Override
+            public void handle(HttpExchange exchange) throws IOException {
+                String response = "Hello World from Docker Java Server!";
+                exchange.sendResponseHeaders(200, response.length());
+                OutputStream os = exchange.getResponseBody();
+                os.write(response.getBytes());
+                os.close();
+            }
+        });
+        System.out.println("Server started on port 5000...");
+        server.start();
+    }
+}
